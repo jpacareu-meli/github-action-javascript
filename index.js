8a5223473d54aebc6d6b464b6b9629aad82e28f5
Javier <javier.pacareu@mercadolibre.cl>
2020-09-19 11:29:53 -0300

    Update files

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-  const nameToGreet = core.getInput("whotogreet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+
+async function getPullRequests() {
+  const client = github.client(process.env.GITHUB_TOKEN);
+  const repo = client.repo(github.context.payload.repository.full_name);
+  const result = await repo.prsAsync({ per_page: 100, state: "open" });
+  return result[0];
 }
+
+async function run() {
+  try {
+    const nameToGreet = core.getInput("whotogreet");
+    console.log(`Hello ${nameToGreet}!`);
+    const prs = await getPullRequests()
+    const prsJSON = JSON.stringify(prs, undefined, 2);
+    console.log(`The OPEN PRs are: ${prsJSON}`);
+    core.setOutput("time", new Date().toTimeString());
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
